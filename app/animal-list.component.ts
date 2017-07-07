66ff34caba08aac96ca2f4732c9f70b515779b93
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-07 12:19:37 -0700

    add select dropdown to animal list component with options for filters

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -4,7 +4,13 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'animal-list-component',
   template: `
-    <h1>All animals:</h1>
+    <h1>Viewing:
+      <select (change)="filterChange($event.target.value)">
+        <option value='allAnimals'>All animals</option>
+        <option value='Adult animals (greater than 2 yrs old)'>Adult animals</option>
+        <option value='Young animals (less than 2 yrs old)'>Young animals</option>
+      </select>
+    </h1>
     <div *ngFor='let animal of childAnimalList' class="well animal-card">
       <h1>{{animal.name}}</h1>
       <h3>{{animal.sex}} {{animal.species}}, age: {{animal.age}}</h3>
@@ -18,8 +24,13 @@ import { Animal } from './animal.model';
 export class AnimalListComponent {
   @Input() childAnimalList: Animal[];
   @Output() editAnimalSender = new EventEmitter();
+  filterType: string = 'allAnimals';
 
   editButtonClicked(animal: Animal): void {
     this.editAnimalSender.emit(animal);
+  }
+
+  filterChange(filterOption: string): void {
+    this.filterType = filterOption;
   }
 }
