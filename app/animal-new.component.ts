4fc92ad085af6c88b37ba8aa4ba1490141b85382
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-07 12:51:12 -0700

    add rudimentary form validation for new animal component

--- app/animal-new.component.ts
+++ app/animal-new.component.ts
@@ -6,28 +6,46 @@ import { Animal } from './animal.model';
   template: `
     <button (click)='addingAnimals = true' *ngIf='!addingAnimals' class='btn'>Add New Animal</button>
     <div *ngIf='addingAnimals' class='well'>
-      <label>Animal name</label>
-      <input #name type="text" class="form-control form-group">
-      <label>Species</label>
-      <input #species type="text" class="form-control form-group">
-      <label>Age</label>
-      <input #age type="number" class="form-control form-group">
-      <label>Diet</label>
-      <input #diet type="text" class="form-control form-group">
-      <label>Exhibit/location</label>
-      <input #location type="text" class="form-control form-group">
-      <label>Number of caretakers</label>
-      <input #caretakers type="number" class="form-control form-group">
-      <label>Sex</label>
-      <select #sex class='form-control form-group'>
+      <div class='form-group'>
+        <label>Animal name</label>
+        <input #name type="text" class="form-control">
+      </div>
+      <div class='form-group'>
+        <label>Species</label>
+        <input #species type="text" class="form-control">
+      </div>
+      <div class='form-group'>
+        <label>Age</label>
+        <input #age type="number" class="form-control">
+      </div>
+      <div class='form-group'>
+        <label>Diet</label>
+        <input #diet type="text" class="form-control">
+      </div>
+      <div class='form-group'>
+        <label>Exhibit/location</label>
+        <input #location type="text" class="form-control">
+      </div>
+      <div class='form-group'>
+        <label>Number of caretakers</label>
+        <input #caretakers type="number" class="form-control">
+      </div>
+      <div class='form-group'>
+        <label>Sex</label>
+        <select #sex class='form-control'>
         <option value='Female'>Female</option>
         <option value='Male'>Male</option>
-      </select>
-      <label>Likes</label>
-      <input #likes type="text" class="form-control form-group">
-      <label>Dislikes</label>
-      <input #dislikes type="text" class="form-control form-group">
-      <button (click)='animalAdded(name.value, species.value, age.value, diet.value, location.value, caretakers.value, sex.value, likes.value, dislikes.value); addingAnimals = false;name.value="";species.value="";age.value="";diet.value="";location.value="";caretakers.value="";sex.value="";likes.value="";dislikes.value=""' class="btn">Add Animal</button>
+        </select>
+      </div>
+      <div class='form-group'>
+        <label>Likes</label>
+        <input #likes type="text" class="form-control">
+      </div>
+      <div class='form-group'>
+        <label>Dislikes</label>
+        <input #dislikes type="text" class="form-control">
+      </div>
+      <button (click)='animalAdded(name.value, species.value, age.value, diet.value, location.value, caretakers.value, sex.value, likes.value, dislikes.value); name.value="";species.value="";age.value="";diet.value="";location.value="";caretakers.value="";sex.value="";likes.value="";dislikes.value=""' class="btn">Add Animal</button>
       <button (click)='addingAnimals = false' class='btn'>Cancel</button>
     </div>
   `
@@ -38,7 +56,10 @@ export class AnimalNewComponent {
   addingAnimals: boolean = false;
 
   animalAdded(name: string, species: string, age: string, diet: string, location: string, caretakers: string, sex: string, likes: string, dislikes: string): void {
-    const newAnimal: Animal = new Animal(name, species, Number(age), diet, location, Number(caretakers), sex, likes, dislikes);
-    this.newAnimalSender.emit(newAnimal);
+    if(name && species && Number(age) && diet && location && Number(caretakers) && sex && likes && dislikes) {
+      const newAnimal: Animal = new Animal(name, species, Number(age), diet, location, Number(caretakers), sex, likes, dislikes);
+      this.newAnimalSender.emit(newAnimal);
+      this.addingAnimals = false;
+    }
   }
 }
